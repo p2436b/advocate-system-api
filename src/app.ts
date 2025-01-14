b09f4c1a05110dd624f04ad878cb06ad26cfc7a0
Peyman Bayat <p2436b@gmail.com>
2025-01-14 15:09:53 +0300

    Initialize database connection on app start

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
+import 'reflect-metadata';
 import express from 'express';
 import clientRoutes from './routes/clients';
 import advocateRoutes from './routes/advocates';
-import "reflect-metadata"
+import { AppDataSource } from './data-source';
 
 const app = express();
 
 app.use(clientRoutes);
 app.use(advocateRoutes);
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+	try {
+		console.log('Initializing database connection...');
+		await AppDataSource.initialize();
+		console.log('Database connection initialized.');
+	} catch (error) {
+		console.error('Could not initialize database connection!', error);
+	}
+
 	console.log('Server is running on http://localhost:3000');
 });
